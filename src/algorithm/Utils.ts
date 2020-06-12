f15954e9646a3c6691cb1ac0d5178c83897aaa8b
doanthihoaithu <hoaithuxt1998@gmail.com>
2020-06-12 23:05:51 +0700

    add for example

--- src/algorithm/Utils.ts
+++ src/algorithm/Utils.ts
@@ -27,3 +27,21 @@ export function abs(a: number) {
     else if (a < 0) return -a;
     else return 0;
 }
+
+export function calculate_discount(prize: number, name: string) {
+    let result = 0;
+    if ( name == "QuangHai") {
+        result = prize*80/100;
+        if (result < 5000000) return result;
+        else {
+            result = result + 200000;
+        }
+    } else if (name == "CongPhuong") {
+        result = prize*50/100;
+        if (result < 5000000) return result;
+        else {
+            result = result + 500000;
+        }
+    }
+    return prize;
+}
